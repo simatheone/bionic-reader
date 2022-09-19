98113b4203bf3cbf003b94e26ab42835542237d8
simatheone <samone07@mail.ru>
2022-09-19 22:43:52 +0300

    Add UserManager

---
+++ app/core/user.py
@@ -0,0 +1,67 @@
+from typing import Union
+
+from fastapi import Depends
+from fastapi_users import (BaseUserManager, FastAPIUsers, IntegerIDMixin,
+                           InvalidPasswordException)
+from fastapi_users.authentication import (AuthenticationBackend,
+                                          CookieTransport, JWTStrategy)
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.config import settings
+from app.core.db import get_async_session
+from app.models.user import User
+from app.schemas.user import UserCreate
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
+
+    async def validate_password(
+            self,
+            password: str,
+            user: Union[UserCreate, User],
+    ) -> None:
+        if len(password) < 8:
+            raise InvalidPasswordException(
+                reason='Password should be at least 8 characters'
+            )
+        if user.email in password:
+            raise InvalidPasswordException(
+                reason='Password should not contain e-mail'
+            )
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
+
+
+cookie_transport = CookieTransport(
+    cookie_max_age=3600,
+    cookie_name='bionic-reader-cookie-auth',
+    cookie_secure=True,
+    cookie_httponly=True
+)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=settings.secret, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name='jwt',
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy
+)
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend]
+)
+
+current_user = fastapi_users.current_user(active=True)
+current_superuser = fastapi_users.current_user(active=True, superuser=True)
