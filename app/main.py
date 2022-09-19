30192e641b9a75478918b122d2d3ec0505a8f4d2
simatheone <samone07@mail.ru>
2022-09-19 22:45:14 +0300

    Add startup event for creating new superuser in db

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
-# from app.core.init_db import create_first_superuser
+from app.core.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.app_title,
@@ -12,6 +12,6 @@ app = FastAPI(
 app.include_router(main_router)
 
 
-# @app.on_event('startup')
-# async def startup():
-#     await create_first_superuser()
+@app.on_event('startup')
+async def startup():
+    await create_first_superuser()
