87ca314d78ab490b0e0a3111a27ccc9ed191fdae
simatheone <samone07@mail.ru>
2022-09-19 22:43:40 +0300

    Add settings for superuser, database_url, secret

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,17 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
     app_title: str = 'Bionic Reader'
     app_description: str = 'Bionic Reader'
-    database_url: str = 'Bionic Reader'
+    database_url: str | None
+    secret: str = 'SECRET'
+    first_superuser_email: EmailStr | None
+    first_superuser_password: str | None
+    first_name_superuser: str | None
 
     class Config:
         env_file = '.env'
 
 
-settings = Settings()
+settings = Settings()   # pyright: ignore
