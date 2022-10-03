520a80a7a354f62f48098111cf2d8aa972fdaa07
simatheone <samone07@mail.ru>
2022-10-03 21:03:33 +0300

    Add settings for postgres

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,11 @@ class Settings(BaseSettings):
     app_title: str = 'Bionic Reader'
     app_description: str = 'Bionic Reader'
     database_url: Optional[str]
+    pgdatabase: Optional[str]
+    pghost: Optional[str]
+    pgpassword: Optional[str]
+    pgport: Optional[str]
+    pguser: Optional[str]
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr]
     first_superuser_password: Optional[str]
