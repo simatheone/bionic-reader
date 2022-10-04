e2cb09c72a1500badb8e2b16012539e540466a6e
simatheone <samone07@mail.ru>
2022-10-04 22:19:06 +0300

    Hardcode cors settings

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
-import os
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 
 from app.api.routers import main_router
 from app.core.config import settings
 from app.core.init_db import create_first_superuser
-
-load_dotenv()
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.app_description
 )
 
-origins = os.getenv('ORIGINS').split(', ')
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=os.getenv('ALLOW_CREDENTIALS'),
-    allow_methods=os.getenv('ALLOW_METHODS').split(','),
-    allow_headers=os.getenv('ALLOW_HEADERS').split(',')
+    allow_origins=[
+        'https://bionic-reader-1g8rrnpz2-bnzone.vercel.app',
+        'https://bionic-reader-nu.vercel.app',
+        'http://localhost',
+        'http://localhost:8080',
+        'http://localhost:3000',
+        'http://127.0.0.1:8000'
+    ],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
 app.include_router(main_router)
