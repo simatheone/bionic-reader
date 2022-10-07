e75098ec25ce081fe02a7d5a28f7042edcdd3577
simatheone <samone07@mail.ru>
2022-10-08 00:17:44 +0300

    Add Document model

---
+++ app/models/document.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, String, Text, DateTime, Integer
+
+from app.core.db import Base
+
+
+class Document(Base):
+    """The Document model."""
+    title = Column(String(256), nullable=False)
+    text = Column(Text, nullable=False)
+    create_date = Column(DateTime, default=datetime.now)
+    user_id = Column(Integer, ForeignKey(
+        'user.id', name='fk_document_user_id_user'
+    ))
+
+    def __repr__(self):
+        return f'Created document: {self.title}'
