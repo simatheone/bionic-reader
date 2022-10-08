8179b43112753612872fece21a4aeb8bbe44eca5
simatheone <samone07@mail.ru>
2022-10-08 16:35:29 +0300

    Refactor Document model, make migration

--- app/models/document.py
+++ app/models/document.py
@@ -7,12 +7,13 @@ from app.core.db import Base
 
 class Document(Base):
     """The Document model."""
-    title = Column(String(256), nullable=False)
+    title = Column(String(256), unique=True, nullable=False)
     text = Column(Text, nullable=False)
-    create_date = Column(DateTime, default=datetime.now)
+    create_date = Column(DateTime, default=datetime.now, nullable=False)
     user_id = Column(Integer, ForeignKey(
         'user.id', name='fk_document_user_id_user'
-    ))
+        ), nullable=False
+    )
 
     def __repr__(self):
         return f'Created document: {self.title}'
