96b886a74b94cbfc9f1cfa7f9bd84db4b0868a8e
simatheone <samone07@mail.ru>
2022-10-08 14:33:48 +0300

    Add import of Document model

--- app/core/base.py
+++ app/core/base.py
@@ -1,2 +1,3 @@
-from app.core.db import Base    # noqa
-from app.models import User     # noqa
+"""Import Base class and all models for Alembic."""
+from app.core.db import Base            # noqa
+from app.models import Document, User   # noqa
