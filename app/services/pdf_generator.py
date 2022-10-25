d6dca9eaa337cc47302b149bdc80aeeb5cdd2e33
simatheone <samone07@mail.ru>
2022-10-25 17:54:57 +0300

    Add new fonts to pdf, add links to githubs, site in footer

--- app/services/pdf_generator.py
+++ app/services/pdf_generator.py
@@ -1,35 +1,48 @@
 from pathlib import Path
 
-from fpdf import FPDF
+from fpdf import FPDF, HTMLMixin
 
 from app.services.text_transformation import execute_transformation_process
 
-BASE_DIR = Path(__file__).parent.parent
-STATIC_FOLDER = BASE_DIR.joinpath('static')
+STATIC_FOLDER = Path('./app').resolve() / 'static'
+FONTS_FOLDER = STATIC_FOLDER / 'fonts'
 
 
-class PDF(FPDF):
+class PDF(FPDF, HTMLMixin):
     def header(self):
         self.image(
-            STATIC_FOLDER.joinpath('pdf_background.png'),
+            STATIC_FOLDER / 'pdf_background.png',
             x=0,
             y=0,
             w=210,
-            h=297
+            h=297,
+            alt_text='grey background'
         )
         self.image(
-            STATIC_FOLDER.joinpath('Bionic-Reader-img.jpg'),
+            STATIC_FOLDER / 'Bionic-Reader-img.jpg',
             x=10,
             y=8,
             w=33,
-            title='Bionic Reader image'
+            alt_text='Bionic Reader image'
         )
         self.ln(20)
 
     def footer(self):
-        self.set_y(-15)
+        self.set_font('Times', 'B', size=8)
+        self.set_x(0)
+        self.set_y(-8)
+        footer_html_text = (
+            '<a href="https://bionic-reader.up.railway.app/">'
+            'Bionic Reader</a><br />Developed by Igor'
+            ' <a href="https://github.com/bnzone">@bnzone</a>'
+            ' & Alexander <a href="https://github.com/simatheone">'
+            '@simatheone</a>'
+        )
+        self.write_html(footer_html_text)
+
+        self.set_y(-7)
         self.set_font('Times', 'B', size=10)
-        self.cell(0, 0, f'{self.page_no()}', align='C')
+        self.cell(0, 0, f'Page {self.page_no()}', align='C')
 
 
 async def execute_pdf_generation_process(
@@ -41,12 +54,27 @@ async def execute_pdf_generation_process(
     )
     new_pdf = PDF()
     new_pdf.add_page()
-    # Add try/except that pdf builds with that text font
-    # pdf.errors.FPDFUnicodeEncodingException: Character "’"
-    # at index 389 in text is outside the range of characters supported
-    # by the font used: "times". Please consider using a Unicode font.
-    # new_pdf.set_font('Times', size=14)
-    new_pdf.set_font('Latin-1', size=14)
+    font_path_inter_regular = (FONTS_FOLDER / 'Inter-Regular.ttf').as_posix()
+    font_path_inter_bold = (FONTS_FOLDER / 'Inter-Bold.ttf').as_posix()
+
+    new_pdf.add_font(
+        fname=font_path_inter_bold,
+        family='Inter',
+        style='B'
+    )
+    new_pdf.add_font(
+        fname=font_path_inter_regular,
+        family='Inter',
+        style=''
+    )
+    new_pdf.set_font(
+        'zapfdingbats',
+        size=14
+    )
+    new_pdf.set_font(
+        'Inter',
+        size=12
+    )
     new_pdf.multi_cell(
         w=0,
         h=10,
