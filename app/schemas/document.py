94e7187715f70c7817621a3adeebb6437e50d271
simatheone <samone07@mail.ru>
2022-11-17 13:09:08 +0300

    add ORMMode class, change classes' inheritance

--- app/schemas/document.py
+++ app/schemas/document.py
@@ -5,6 +5,11 @@ from uuid import UUID
 from pydantic import BaseModel, Extra, Field
 
 
+class ORMMode(BaseModel):
+    class Config:
+        orm_mode = True
+
+
 class DocumentBaseSchema(BaseModel):
     title: Optional[str] = Field(None, min_length=1, max_length=256)
     text: Optional[str] = Field(None, min_length=1)
@@ -30,17 +35,10 @@ class DocumentTransformRequest(BaseModel):
     text: str = Field(min_length=1)
 
 
-class DocumentInfo(DocumentCreate):
+class DocumentResponse(DocumentCreate, ORMMode):
     id: UUID
+
+
+class DocumentInfo(DocumentResponse):
     create_date: datetime
     user_id: Optional[UUID]
-
-    class Config:
-        orm_mode = True
-
-
-class DocumentResponse(DocumentCreate):
-    id: UUID
-
-    class Config:
-        orm_mode = True
