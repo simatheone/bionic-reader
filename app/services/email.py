4f125b14a6df15f5ba8de2e7b8153c805b081e1d
simatheone <samone07@mail.ru>
2023-02-20 21:36:17 +0300

    feat: add service email, add logic for forgot and reset password

---
+++ app/services/email.py
@@ -0,0 +1,27 @@
+from smtplib import SMTP_SSL, ssl
+from typing import Tuple
+
+from app.db.settings import settings
+
+
+def get_smtp_configuration() -> Tuple[str, int, str, str]:
+    """Read and return configurations for smtp server."""
+    smtp_server = settings.SMTP_SERVER
+    port = settings.SMTP_PORT
+    sender_email = settings.SENDER_EMAIL
+    mail_password = settings.MAIL_PASSWORD
+    return (smtp_server,
+            port,
+            sender_email,
+            mail_password)
+
+
+async def send_email(receiver_email: str, message: str) -> None:
+    """Service function for sending email."""
+    context = ssl.create_default_context()
+    smtp_server, port, sender_email, mail_password = (
+        get_smtp_configuration()
+    )
+    with SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, mail_password)
+        server.sendmail(sender_email, receiver_email, message)
